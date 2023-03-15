cd8d9792988342f8fe55028b2d323c182c3c8f07
facundo cajal <facundocajal199@gmail.com>
2023-03-15 16:10:55 -0300

    se realizo el ejercicio2 de funciones

--- js/arreglos/3-ejercicio.js
+++ js/arreglos/3-ejercicio.js
@@ -1,4 +1,5 @@
-/*3- Escribir un script que simule el lanzamiento de dos dados. Hacer uso de la función Math.random para obtener números aleatorios entre 1 y 6 para cada uno de los lanzamientos de los dados. Sumar el resultado de lanzar dos dados y anotar en un array el número de apariciones de dicha suma, repitiendo 50 veces esta operación.*/
+/*3- Escribir un script que simule el lanzamiento de dos dados. Hacer uso de la función Math.random para obtener números aleatorios entre 1 y 6 para cada uno de los lanzamientos de los dados. Sumar el resultado de lanzar dos dados y anotar en un array el número de apariciones de dicha suma, repitiendo 50 veces esta operación. 
+
 
 
 
@@ -30,3 +31,4 @@ for (let i = 0; i < resultados.length; i++) {
   document.write(`<tr><td>${suma}</td><td>${repeticiones}</td></tr>`);
 }
 document.write("</table>");
+*/
--- js/funciones/2-ejercicio.js
+++ js/funciones/2-ejercicio.js
@@ -1,2 +1,20 @@
 /*2- Definir una función que muestre información sobre una cadena de texto que se le pasa como argumento. A partir de la cadena que se le pasa, la función determina si esa cadena está formada sólo por mayúsculas, sólo por minúsculas o por una mezcla de ambas.
 */
+function mostrarInformacion(cadena) {
+    if (cadena === cadena.toUpperCase()) {
+      document.write("<h2>La cadena está formada solo por mayúsculas</h2>.");
+    } else if (cadena === cadena.toLowerCase()) {
+      document.write("<h2>La cadena está formada solo por minúsculas</h2>");
+    } else {
+      document.write("<h2>La cadena está formada por una mezcla de mayúsculas y minúsculas.</h2>");
+    }
+  }
+  
+  let cadena = prompt("Introduce una cadena de texto:");
+  mostrarInformacion(cadena);
+
+  //usamos una variable con texto
+  // usamos esa variable y la comparamos con letras mayusculas si es exactammente igual lanza un mensaje
+  //hacemos lo mismo abajo
+  // y de lo contrario es una mescla
+   
